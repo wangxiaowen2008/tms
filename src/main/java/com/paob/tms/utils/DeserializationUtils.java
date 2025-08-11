3f2829a4518496c6b2df150a0202e6b8306fa452
wangxiaowen <279147969@qq.com>
2025-08-11 16:39:39 +0800

    update12

--- src/main/java/com/paob/tms/utils/DeserializationUtils.java
+++ src/main/java/com/paob/tms/utils/DeserializationUtils.java
@@ -1,5 +1,5 @@
 public class DeserializationUtils {
-    
+    // 反序列化对象，有漏洞，可以导致远程代码执行
     public Object deserializeObject(byte[] data) {
         try {
             ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
