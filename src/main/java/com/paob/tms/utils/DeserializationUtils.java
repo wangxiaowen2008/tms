9e815d998a08258cf8b6944be2e8bd92d63f722e
wangxiaowen <279147969@qq.com>
2025-08-11 16:35:52 +0800

    update11

--- src/main/java/com/paob/tms/utils/DeserializationUtils.java
+++ src/main/java/com/paob/tms/utils/DeserializationUtils.java
@@ -1,9 +1,7 @@
 public class DeserializationUtils {
     
-    // 不安全的反序列化风险
     public Object deserializeObject(byte[] data) {
         try {
-            // 危险：不安全的反序列化
             ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
             return ois.readObject();
         } catch (Exception e) {
